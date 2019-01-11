4f1ed0b122309b09668afdc5a7c48a5a290c63cb
Michael Kelly <mike.s.kelly@icloud.com>
2019-01-11 10:21:23 -0800

    Inital setup

--- Lab03WordGuessGame/Program.cs
+++ Lab03WordGuessGame/Program.cs
@@ -6,7 +6,45 @@ namespace Lab03WordGuessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
         }
+
+        /// <summary>
+        /// This is where I create the menu for choosing whether to play, administrate, or exit.
+        /// </summary>
+        static bool MainMenu()
+        {
+            try
+            {
+                string gameMessage = "Play the game";
+                Console.Clear();
+                Console.WriteLine("Please Choose an option");
+                Console.WriteLine($"1) {gameMessage}");
+                Console.WriteLine("2) Administrative");
+                Console.WriteLine("3) Exit");
+                string result = Console.ReadLine();
+                if (result == "1")
+                {
+                    gameMessage = "Play the game, again.";
+
+
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+            }
+        }
+
     }
 }
